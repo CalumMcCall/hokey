fb4f29dccd5a2a318a11d4422b4cfae7f42f230a
Calum McCall <mccallcalum@gmail.com>
2015-04-04 17:39:03 +0100

    Added first few hand eval functions.

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
-data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace 
-data Suit = Spades | Diamonds | Clubs | Hearts
+import Data.List
+
+data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Ord, Eq)
+data Suit = Spades | Diamonds | Clubs | Hearts 
 data Card = Card Rank Suit 
 
 instance Show Rank where
@@ -24,10 +26,40 @@ instance Show Suit where
     show Hearts = "h"
 
 instance Show Card where
-    show (Card y x) = (show y) ++ (show x)
+    show (Card r s) = (show r) ++ (show s)
+
+instance Ord Card where
+    (Card r1 s1) `compare` (Card r2 s2) = r1 `compare` r2
+
+instance Eq Card where
+    (Card r1 s1) == (Card r2 s2) = r1 == r2
 
 getSuit :: Card -> Suit
 getSuit (Card _ s) = s
 
 getRank :: Card -> Rank
 getRank (Card r _) = r
+
+hasPair :: [Card] -> Maybe [Card]
+hasPair (x:y:xs)
+    | x == y    = Just (x:y:[])
+    | otherwise = hasPair (y:xs)
+hasPair _       = Nothing
+
+hasTrips :: [Card] -> Maybe [Card]
+hasTrips (x:y:z:xs)
+    | x == y && x == z  = Just (x:y:z:[])
+    | otherwise             = hasTrips (y:z:xs)
+hasTrips _                  = Nothing
+
+hasQuads :: [Card] -> Maybe [Card]
+hasQuads (w:x:y:z:zs)
+    | w == x && w == y && w == z    = Just (w:x:y:z:[])
+    | otherwise                     = hasQuads (x:y:z:zs)
+hasQuads _                          = Nothing
+
+hasFullHouse :: [Card] -> Maybe [Card]
+hasFullHouse cards = case hasPair cards of Nothing  -> Nothing
+                                           Just x   -> case trips of Nothing    -> Nothing
+                                                                     Just y     -> Just (y ++ x)
+                                                       where trips = hasTrips (filter (`notElem` x) cards)
