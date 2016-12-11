d539081067ee7833f48cfae8b52353e27985ea91
Calum McCall <calum@advancedtelematic.com>
2016-12-11 19:23:59 +0100

    Added function to compare hand to range

--- src/Hokey/Odds.hs
+++ src/Hokey/Odds.hs
@@ -1,14 +1,30 @@
-module Hokey.Odds (HoleCards, deck, remainingCards) where
+module Hokey.Odds (HoleCards, deck, remainingCards, compareHandToRange) where
 
 import           Hokey.Card
+import           Hokey.Hand
 
 type HoleCards = (Card, Card)
 
--- takes two player ranges and a board and returns the number of times player 1 wins and player 2
--- wins
-getOdds :: [HoleCards] -> [HoleCards] -> [Card] -> (Integer, Integer)
-getOdds p1 p2 boardCards = (0, 0)
+type HandResults = (Integer, Integer, Integer)
 
+compareHandToRange :: [Card] -> [Card] -> [[Card]] -> HandResults
+compareHandToRange hand board range = foldl (compareTwoHands hand board) (0, 0, 0) range
+
+compareTwoHands :: [Card] -> [Card] -> HandResults -> [Card] -> HandResults
+compareTwoHands hand1 board (p1, d, p2) hand2 =
+  case (getBestHand (hand1 ++ board), getBestHand (hand2 ++ board)) of
+    (Just h1, Just h2) ->
+      case (h1 `compare` h2) of
+        GT -> (p1 + 1, d, p2)
+        LT -> (p1, d, p2 + 1)
+        EQ -> (p1, d + 1, p2)
+    (_, _) -> error "invalid hands given"
+
+-- takes two player ranges and a board and returns the number of times player 1 wins and player 2
+-- wins getOdds :: [HoleCards] -> [HoleCards] -> [Card] -> (Integer, Integer) getOdds p1 p2
+-- boardCards = (p2Wins, p2Wins)
+--  where
+--    p1
 ranks :: [Rank]
 ranks = [Deuce .. Ace]
 
--- test/OddsSpec.hs
+++ test/OddsSpec.hs
@@ -9,6 +9,12 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  let redAces = [Card Ace H, Card Ace D]
+      blackAces = [Card Ace S, Card Ace C]
+      blankBoard = [Card King H, Card Five C, Card Queen S, Card Deuce C, Card Nine C]
+      blackKings = [Card King S, Card King C]
+      blackQueens = [Card Queen S, Card Queen C]
+      redJacks = [Card Jack S, Card Jack C]
   describe "remainingCards" $ do
     it "removes correct card" $ do
       remainingCards (Card Deuce S : []) `shouldSatisfy` ranksEqual (tail deck)
@@ -18,3 +24,13 @@ spec = do
                                                                                   (\x ->
                                                                                      x /= Card Ace C)
                                                                                   deck)
+
+  describe "compareHandToRange" $ do
+    it "recognises draws" $ do
+      compareHandToRange redAces blankBoard [blackAces] `shouldBe` (0, 1, 0)
+    it "recognises winning hands" $ do
+      compareHandToRange redAces blankBoard [redJacks] `shouldBe` (1, 0, 0)
+    it "recognises losing hands" $ do
+      compareHandToRange redAces blankBoard [blackKings] `shouldBe` (0, 0, 1)
+    it "recognises losing ranges" $ do
+      compareHandToRange redAces blankBoard [blackKings, blackQueens, redJacks] `shouldBe` (1, 0, 2)
